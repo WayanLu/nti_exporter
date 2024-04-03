97f143a5c03e5c228d6802c799ee506fb31f4d4a
WayanLu <ryliu3@gmail.com>
2024-04-03 11:13:42 -1000

    added parsing function for config.json in nti_exporter.py

--- nti_exporter.py
+++ nti_exporter.py
@@ -1,23 +1,34 @@
-import random
 import time
+import json
 from datetime import datetime
 from prometheus_client import start_http_server, Gauge
 from readNTIsensors import get_nti_sensors
 
-# Interval for pulling data
-INTERVAL = 5
-PORT = 8103
-ADDRESS = 'http://10.0.0.11'
-
 # Add more variables if you want to exporter more metrics
 temperature_gauge = Gauge('nti_sensor_temperature', 'Temperature of NTI sensors', ['sensor_id'])
 
-def process_request(t):
+def read_config(filepath):
+    '''
+    Process yaml file to get config
+    '''
+    with open(filepath, 'r') as file:
+        config_json = file.read()
+
+    config = json.loads(config_json)
+    print(config)
+    (address, interval, port) = (
+            config["address"],
+            int(config["interval"]),
+            int(config["port"])
+    )
+
+    return address, interval, port
+def process_request(t, addr):
     '''
     Get data from NTI sensors and update exporter
     '''
 
-    nti_data = get_nti_sensors(ADDRESS)
+    nti_data = get_nti_sensors(addr)
     print(f'{datetime.now()} nti_sensor_data: {nti_data}')
     for (sensor_id, temperature) in nti_data.items():
         temperature_gauge.labels(sensor_id=sensor_id).set(temperature)
@@ -25,10 +36,16 @@ def process_request(t):
     time.sleep(t)
 
 if __name__ == '__main__':
+
+    filepath = "config.json"
+    address, interval, port = read_config(filepath)
+    
     # Start up the server to expose the metrics.
-    start_http_server(PORT)
-    print(f'{datetime.now()} Start nti_exporter listening on port {PORT}...')
+    start_http_server(port)
+    print(f'{datetime.now()} Start nti_exporter listening on port {port}...')
     # Generate some requests.
     while True:
         print(f'{datetime.now()} Retrieving data from NTI sensors...')
-        process_request(INTERVAL)
+        process_request(interval)
+
+
