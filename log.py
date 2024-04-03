c18ea0395ea3b609b7331be4160c056db297daef
WayanLu <ryliu3@gmail.com>
2024-04-03 12:19:39 -1000

    added log.py for logging

---
+++ log.py
@@ -0,0 +1,20 @@
+import logging
+
+
+def set_logger() -> None:
+    # Configure logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    # Create file handler which logs messages to a file
+    file_handler = logging.FileHandler('nti_exporter.log')
+    file_handler.setLevel(logging.INFO)
+    # Create console handler which logs messages to the console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    # Create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
